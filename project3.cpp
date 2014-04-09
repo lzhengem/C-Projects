dc70abbcff47f52fe6666ae8d20ea64476c4bec3
mimi <xlightsaccount@hotmail.com>
2014-04-09 02:34:12 -0700

    Add project3.cpp

---
+++ project3.cpp
@@ -0,0 +1,24 @@
+/*
+cryptic words
+*/
+
+#include <iostream>
+using namespace std;
+
+message_scrambler(char message)
+
+int main()
+
+{
+	char message[51];
+	cout << "What would you like your message to be?";
+	cin >> message;
+	cout << "Your message is \" " << message << " \". " << endl;
+
+}
+
+message_scrambler(char message)
+{
+	[message];
+
+}
